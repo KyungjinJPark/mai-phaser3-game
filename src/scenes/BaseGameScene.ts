4518038dbf9e64b7ac19105666a31d397221e111
KyungjinJPark <kjparkcontact@gmail.com>
2022-05-05 02:08:41 -0400

    `CurrentSceneManager` stays updated on scene transition

--- src/scenes/BaseGameScene.ts
+++ src/scenes/BaseGameScene.ts
@@ -24,6 +24,9 @@ export abstract class BaseGameScene extends Phaser.Scene {
   public create() {
     // Initialize managers
     CurrentSceneManager.scene = this // `this` refers to the child. Not sure if this is just for abstract classes or all parent classes.
+    this.events.on('wake', () => {
+      CurrentSceneManager.scene = this
+    })
     this.inputManager = new InputManager(this.input)
     this.objectManager = new ObjectManager()
     this.objects = []
